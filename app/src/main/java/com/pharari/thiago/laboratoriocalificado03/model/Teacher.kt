fea738fbe1628f73e7db1906fd47a5954fbfdd15
ThiagoPharari <thiago.pharari@tecsup.edu.pe>
2024-11-16 10:35:53 -0500

    lab calificado Pharari Thiago

---
+++ app/src/main/java/com/pharari/thiago/laboratoriocalificado03/model/Teacher.kt
@@ -0,0 +1,9 @@
+package com.pharari.thiago.laboratoriocalificado03.model
+
+data class Teacher(
+    val name: String,
+    val lastName: String,
+    val phoneNumber: String,
+    val email: String,
+    val imageUrl: String
+)
